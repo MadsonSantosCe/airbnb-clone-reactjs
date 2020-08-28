84bf359804710e290254c9e94c61d589c214c212
Madson Santos <madsonsamsung@gmail.com>
2020-08-28 11:03:03 -0300

    Criação do Header

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      <Home />
+
+      {/*HOME*/}
+
+        {/*Header*/}
+
+        {/*Banner*/}
+          {/*Search*/}
+
+        {/*Cards*/}
+
+        {/*Footer*/}
+      
+      {/*SEARCHPAGE*/}
+      
+    </div>
+  );
+}
+
+export default App;
